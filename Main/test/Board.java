c6e9c4b61562a6c964d54ad230802b00f8eec399
shahareldar <88107120+shahareldar@users.noreply.github.com>
2024-08-13 14:31:36 +0300

    neword -> newword

--- Main/test/Board.java
+++ Main/test/Board.java
@@ -39,6 +39,40 @@ public class Board {
         return string + "]";
     }
 
+    private static void addPositions(Set<String> set, int[][] positions) {
+        for (int[] pos : positions) {
+            set.add(pos[0] + "," + pos[1]);
+        }
+    }
+
+    static {
+        // Initialize the sets with the bonus positions
+        addPositions(tripleWordScore, new int[][]{{0, 0}, {0, 7}, {0, 14}, {7, 0}, {7, 14}, {14, 0}, {14, 7}, {14, 14}});
+        addPositions(doubleWordScore, new int[][]{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {10, 10}, {11, 11}, {12, 12}, {13, 13}, {14, 1}, {13, 1}, {1, 13}, {12, 2}, {12, 2}, {11, 3}, {10, 4}, {4, 10}, {3, 11}});
+        addPositions(tripleLetterScore, new int[][]{{1, 5}, {1, 9}, {5, 1}, {5, 5}, {5, 9}, {5, 13}, {9, 1}, {9, 5}, {9, 9}, {9, 13}, {13, 5}, {13, 9}});
+        addPositions(doubleLetterScore, new int[][]{{0, 3}, {0, 11}, {2, 6}, {2, 8}, {3, 0}, {3, 7}, {3, 14}, {6, 2}, {6, 6}, {6, 8}, {6, 12}, {7, 3}, {7, 11}, {8, 2}, {8, 6}, {8, 8}, {8, 12}, {11, 0}, {11, 7}, {11, 14}, {12, 6}, {12, 8}, {14, 3}, {14, 11}});
+    }
+
+    // Methods to check if a given position is a bonus tile
+    private static boolean isTripleWordScore(int row, int col) {
+        return tripleWordScore.contains(row + "," + col);
+    }
+    
+    private static boolean isDoubleWordScore(int row, int col) {
+        if (isFirst && row == STAR && col == STAR) {
+            return true;
+        }
+        return doubleWordScore.contains(row + "," + col);
+    }
+    
+    private static boolean isTripleLetterScore(int row, int col) {
+        return tripleLetterScore.contains(row + "," + col);
+    }
+    
+    private static boolean isDoubleLetterScore(int row, int col) {
+        return doubleLetterScore.contains(row + "," + col);
+    }
+
     public static Board getBoard() {
         if (singleBoard == null) {singleBoard = new Board();}
         return singleBoard;
@@ -228,40 +262,6 @@ public class Board {
         return newWords;
     }
 
-    private static void addPositions(Set<String> set, int[][] positions) {
-        for (int[] pos : positions) {
-            set.add(pos[0] + "," + pos[1]);
-        }
-    }
-
-    static {
-        // Initialize the sets with the bonus positions
-        addPositions(tripleWordScore, new int[][]{{0, 0}, {0, 7}, {0, 14}, {7, 0}, {7, 14}, {14, 0}, {14, 7}, {14, 14}});
-        addPositions(doubleWordScore, new int[][]{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {10, 10}, {11, 11}, {12, 12}, {13, 13}, {14, 1}, {13, 1}, {1, 13}, {12, 2}, {12, 2}, {11, 3}, {10, 4}, {4, 10}, {3, 11}});
-        addPositions(tripleLetterScore, new int[][]{{1, 5}, {1, 9}, {5, 1}, {5, 5}, {5, 9}, {5, 13}, {9, 1}, {9, 5}, {9, 9}, {9, 13}, {13, 5}, {13, 9}});
-        addPositions(doubleLetterScore, new int[][]{{0, 3}, {0, 11}, {2, 6}, {2, 8}, {3, 0}, {3, 7}, {3, 14}, {6, 2}, {6, 6}, {6, 8}, {6, 12}, {7, 3}, {7, 11}, {8, 2}, {8, 6}, {8, 8}, {8, 12}, {11, 0}, {11, 7}, {11, 14}, {12, 6}, {12, 8}, {14, 3}, {14, 11}});
-    }
-
-    // Methods to check if a given position is a bonus tile
-    private static boolean isTripleWordScore(int row, int col) {
-        return tripleWordScore.contains(row + "," + col);
-    }
-    
-    private static boolean isDoubleWordScore(int row, int col) {
-        if (isFirst && row == STAR && col == STAR) {
-            return true;
-        }
-        return doubleWordScore.contains(row + "," + col);
-    }
-    
-    private static boolean isTripleLetterScore(int row, int col) {
-        return tripleLetterScore.contains(row + "," + col);
-    }
-    
-    private static boolean isDoubleLetterScore(int row, int col) {
-        return doubleLetterScore.contains(row + "," + col);
-    }
-
     public int getScore(Word word) {
         int score = 0;
         Tile[] tiles = word.getTiles();
@@ -306,11 +306,11 @@ public class Board {
 
         score += getScore(word);     
         
-        for (Word neWord : getWords(word)) {
-            if (dictionaryLegal(neWord)) {
-                if (!(boardWords.contains(neWord))) {
-                    boardWords.add(neWord);
-                    score += getScore(neWord);
+        for (Word newWord : getWords(word)) {
+            if (dictionaryLegal(newWord)) {
+                if (!(boardWords.contains(newWord))) {
+                    boardWords.add(newWord);
+                    score += getScore(newWord);
                 }
             } else {
                 return 0;
